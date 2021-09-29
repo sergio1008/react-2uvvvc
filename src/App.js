5d58649ee3382be75a9bf66c9f5c53ece3d917ff
sergio1008 <sergiobernalc@outlook.com>
2021-09-29 16:50:17 -0500

    2 files modified, 4 deleted

--- src/App.js
+++ src/App.js
@@ -1,29 +1,26 @@
 import React from 'react';
 import './style.css';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import NavBar from './components/navbar/NavBar';
 
-import Home from './pages/home/Home';
-import About from './pages/about/About';
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: 'Sergio',
+    };
+    this.changeName = this.changeName.bind(this);
+  }
+
+  changeName() {
+    this.setState((oldState) => ({
+      name: oldState.name + ' ' + 'Bernal',
+    }));
+  }
   render() {
     return (
-      <div>
-        <NavBar />
-        <Router>
-          <Switch>
-          <Route path="/">
-              <Home/>
-            </Route>
-            <Route path="/home">
-              <Home/>
-            </Route>
-            <Route path="/about">
-              <About/>
-            </Route>
-          </Switch>
-        </Router>
-      </div>
+      <>
+        <button onClick={this.changeName}>cambiar</button>
+        <h1>{this.state.name + (this.props.mensaje ? this.props.mensaje : '' )}</h1>
+      </>
     );
   }
 }
