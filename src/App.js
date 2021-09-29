83ed101907bdd1d83fccf53e30babee68beb96e8
sergio1008 <sergiobernalc@outlook.com>
2021-09-29 15:36:20 -0500

    1 file modified

--- src/App.js
+++ src/App.js
@@ -4,6 +4,6 @@ import './style.css';
 import { NavBar } from './components/navbar/NavBar';
 export default class App extends React.Component {
   render() {
-    return <NavBar />;
+    return <NavBar/>;
   }
 }
